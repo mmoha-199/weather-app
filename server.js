c492732c7da5e9515b7b6eafe1f531458fb08d1b
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-07-24 23:05:33 +0100

    copy comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* Empty JS object to act as endpoint for all routes */
-projectData = {};
+weathData = {};
 
 // Express to run server and routes
 const express = require('express');
@@ -21,9 +21,47 @@ app.use(express.static('website'));
 
 const port = 3000;
 
-// TODO-Spin up the server
+// Spin up the server
 const server = app.listen(port, listening);
 function listening(){
     console.log('server running');
     console.log(`running on localhost:${port}`);
 };
+//GET
+app.get('/url', (req, res)=> {
+    res.send(weathData);
+});
+//POST
+//create a POST route that uses the url /add and sends the response POST received when used to make a request.
+app.post('/add', callBack);
+  
+function callBack(req,res){
+  res.send('POST received');
+};
+//Add a POST route for adding a favorite animal via the path ’/animal’ to an array named data.
+const data = [];
+app.post('/animal', sendAnimal);
+function sendAnimal(req,res){
+    data.push(req.body);
+}
+// Setup empty JS object to act as endpoint for all routes
+// Express to run server and routes
+
+// Start up an instance of app
+
+/* Dependencies */
+/* Middleware*/
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+
+// Spin up the server
+// Callback to debug
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
--- website/app.js
+++ website/app.js
@@ -1,21 +1,37 @@
-let baseURL = 'api.openweathermap.org/data/2.5/weather?q='
-let apiKey = '5825686540fc9030090ec177f22c9e72';
-const searchBTN = document.getElementById('search-btn');
-
-searchBTN.addEventListener('click', cityNfunc);
-const cityNfunc = async (baseURL, city, key)=>{
- 
-    const res = await fetch(baseURL+city+key)
-    try {
-
-      const data = await res.json();
-      console.log(data)
-      // 1. We can do something with our returned data here-- like chain promises!
-
-      // 2. 
-      // postData('/addAnimal', data)
-    }  catch(error) {
-      // appropriately handle the error
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
       console.log("error", error);
-    }
-  }
+      }
+}
+let baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+let apiKey = '5825686540fc9030090ec177f22c9e72';
+  
+// Personal API Key for OpenWeatherMap API
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
+
+  
+ 
